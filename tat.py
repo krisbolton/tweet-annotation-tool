f43661b8fb1fed5880fce79b90f435233e6d1f58
krisbolton <krisbolton@users.noreply.github.com>
2021-01-20 13:52:30 +0000

    added banner
    added welcome banner. Altered --file prompt text. added non-0 integer to exit(). Altered version number

--- tat.py
+++ tat.py
@@ -1,5 +1,5 @@
 """ Tweet Annotation Tool 
-	Version: 0.1a
+	Version: 0.1.0-alpha
     Author: Kris Bolton
 	GitHub: github.com/krisbolton
 	Description: CLI tool to annotate tweets with sentiment to create sentiment analysis datasets.
@@ -11,39 +11,39 @@
 import sys
 import os.path
 import click
+import pyfiglet
 
 @click.command()
-@click.option('--file', prompt='Enter file name', help='Then name of the CSV file to read.')
+@click.option('--file', prompt='Enter the csv file to annotate', help='The name of the CSV file to read.')
 def get_file_name(file):
+    """ Get file name from flag (--file) or request file name on start """
     try:
         assert file.endswith('.csv')
     except AssertionError:
         print('Error: file suffix not .csv. Please provide the correct file type.')
-        sys.exit()
+        sys.exit(1)
     try:
         assert os.path.isfile(file)
     except AssertionError:    
         print ('Error: %s file does not exist.' % file)
-        sys.exit()
+        sys.exit(1)
     else:
         print('File %s selected.' % file)
     file_name = file
     return file_name
 
-
-# TODO
-
-#def make_CSV_copy():
-	# chekc if a copy exists. make a copy of the CSV file?
-
-# Mesages (printed to the screen)
-
-#def banner_message():
-
-#def instructions_message():
-
+def welcome_banner():
+    """ Prints welcome banner upon program start """
+    for x in range(5):
+        print('\r\n')
+    figlet = pyfiglet.figlet_format('Tweet Annotation Tool', font='slant')
+    print(figlet)
+    print('by Kris Bolton')
+    print('v0.1.0-alpha')
+    print('\r\n')
 
 def main():
+    welcome_banner()
     get_file_name()
 
 if __name__ == '__main__':
